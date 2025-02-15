ee4dee6ad97387164c21a0cf6846954860c66501
Alinka8 <alinazoldubaeva@gmail.com>
2025-02-14 23:45:30 -0600

    added props

--- src/index.js
+++ src/index.js
@@ -4,24 +4,51 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 //setup vars
-const author = "Mel Robbins";
-const title = "The Let them Theoryyyyy";
-const img =
-  "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL900_SR900,600_.jpg";
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL900_SR900,600_.jpg",
+  title: "The let them theory",
+  author: "Mel Robbins",
+};
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/419CqGgAdZL._SY445_SX342_.jpg",
+  title: "Atomic Habits",
+  author: "James Clear",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          dolorem nobis corrupti ullam ea dicta, accusamus eaque quae aspernatur
+          quo quibusdam quasi, dolore libero quaerat, dolores voluptas! Aliquid,
+          quas labore!
+        </p>
+      </Book>
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
+  console.log(props);
   return (
     <article className="book">
       <img src={img} alt="" className="image" />
       <h1> {title}</h1>
       <h4>{author}</h4>
+      {props.children}
     </article>
   );
 };
