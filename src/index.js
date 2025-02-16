79175d51405aec914b108be26d02e83371d8dd5d
Alinka8 <alinazoldubaeva@gmail.com>
2025-02-15 23:49:55 -0600

    added props

--- src/index.js
+++ src/index.js
@@ -4,51 +4,45 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 //setup vars
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL900_SR900,600_.jpg",
-  title: "The let them theory",
-  author: "Mel Robbins",
-};
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/419CqGgAdZL._SY445_SX342_.jpg",
-  title: "Atomic Habits",
-  author: "James Clear",
-};
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL900_SR900,600_.jpg",
+    title: "The let them theory",
+    author: "Mel Robbins",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/419CqGgAdZL._SY445_SX342_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wZXiu4OiL._AC_UL210_SR195,210_.jpg",
+    title: "The psychology of money",
+    author: "Morgan Housel",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-          dolorem nobis corrupti ullam ea dicta, accusamus eaque quae aspernatur
-          quo quibusdam quasi, dolore libero quaerat, dolores voluptas! Aliquid,
-          quas labore!
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
-  console.log(props);
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props.book;
+
   return (
     <article className="book">
       <img src={img} alt="" className="image" />
       <h1> {title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
