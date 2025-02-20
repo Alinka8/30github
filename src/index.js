b4699f0f14406dfc2f88b4110d7a4f08c741512b
Alinka8 <alinazoldubaeva@gmail.com>
2025-02-19 23:16:08 -0600

    structured

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { data } from "./books";
+import Book from "./Book";
+import { greetings } from "./testing/testing";
 
 //setup vars
 
 function BookList() {
+  console.log(greetings);
   return (
     <section className="booklist">
       {data.map((book) => {
@@ -16,40 +19,6 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  //attribute, eventHandler
-  //onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert("hello world");
-  };
-
-  const complexExample = (author) => {
-    console.log(author);
-  };
-
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt="" className="image" />
-      <h1 onClick={() => console.log(title)}> {title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        {" "}
-        reference example
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        more complex example
-      </button>
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
