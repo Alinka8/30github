c4c728ba1cc6a4dc5ecd51aee49f0ed98249e75b
Alinka8 <alinazoldubaeva@gmail.com>
2025-02-25 23:34:42 -0600

    added js

--- src/javascript.js
+++ src/javascript.js
@@ -1,3 +1,17 @@
 const numbers = [2,10,30,40]
 const newArr = numbers.map(Math.sqrt)
 print(newArr)
+
+// Function is called, the return value will end up in x
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}
+
+function toCelsius(fahrenheit) {
+  return (5 / 9) * (fahrenheit - 32);
+}
+
+let value = toCelsius(77);
