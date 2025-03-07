8df9506dbed24190ba5f3298c4e68e4dec3aa4fc
Alinka8 <alinazoldubaeva@gmail.com>
2025-03-06 23:09:48 -0600

    added js

--- src/books.js
+++ src/books.js
@@ -35,4 +35,22 @@ export const data = [
     title: "The psychology of money",
     author: "Morgan Housel",
   },
+  {
+    id: 7,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL900_SR900,600_.jpg",
+    title: "The let them theory",
+    author: "Mel Robbins",
+  },
+  {
+    id: 8,
+    img: "https://m.media-amazon.com/images/I/419CqGgAdZL._SY445_SX342_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    id: 9,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wZXiu4OiL._AC_UL210_SR195,210_.jpg",
+    title: "The psychology of money",
+    author: "Morgan Housel",
+  },
 ];
